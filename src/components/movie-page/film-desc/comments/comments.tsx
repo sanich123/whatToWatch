a19cb6ace4508da86358c16f64b6b96d25078a58
sanich123 <aovoronin.piano@gmail.com>
2022-02-11 08:58:24 +0300

    Comments are sliced to six copies

--- src/components/movie-page/film-desc/comments/comments.tsx
+++ src/components/movie-page/film-desc/comments/comments.tsx
@@ -5,10 +5,16 @@ import { reviewsReducer } from '../../../../utils/utils';
 import CommentRow from './comments-row';
 import './comments-styles.css';
 
+const NUMBER_SLICE = 6;
+
 export default function Reviews({id}: {id: number}):JSX.Element {
   const reviews = useSelector(({film}: RootState) => film.comments);
   const comments = useComments(id);
   const reviewComms = reviewsReducer([...reviews, ...comments]);
 
-  return <CommentRow reviews={reviewComms} />;
+  const slicedComments = reviewComms.length > NUMBER_SLICE ?
+    reviewComms.slice(reviewComms.length - NUMBER_SLICE, reviewComms.length) :
+    reviewComms;
+
+  return <CommentRow reviews={slicedComments} />;
 }
