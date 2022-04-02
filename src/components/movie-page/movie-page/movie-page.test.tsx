6e64bfadecc7638dac297282999ac9d61992199d
sanich123 <aovoronin.piano@gmail.com>
2022-04-02 22:44:46 +0300

    Some refactoring

--- src/components/movie-page/movie-page/movie-page.test.tsx
+++ src/components/movie-page/movie-page/movie-page.test.tsx
@@ -1,11 +1,6 @@
 import { renderWithProviders } from '../../../test/test-utils';
 import MoviePage from './movie-page';
-import fetchMock from 'jest-fetch-mock';
 
-fetchMock.enableMocks();
-beforeEach((): void => {
-  fetchMock.resetMocks();
-});
 describe('MoviePage component', () => {
   it('renderWithProviders correctly', async () => {
     renderWithProviders(<MoviePage />);
--- src/components/sign-in/sign-in/sign-in.test.tsx
+++ src/components/sign-in/sign-in/sign-in.test.tsx
@@ -1,23 +1,19 @@
-import {render, screen} from '@testing-library/react';
-import { Provider } from 'react-redux';
-import { MemoryRouter } from 'react-router-dom';
+import { screen} from '@testing-library/react';
 import SignIn from './sign-in';
-import { setupStore } from '../../../store/store';
+import { renderWithProviders } from '../../../test/test-utils';
+import userEvent from '@testing-library/user-event';
 
 describe('Sign-in component', () => {
   it('component should render correctly', () => {
-    render(
-      <Provider store={setupStore()}>
-        <MemoryRouter>
-          <SignIn />
-        </MemoryRouter>
-      </Provider>,
-    );
+    renderWithProviders(<SignIn />);
     expect(screen.getAllByText(/sign in/i)).toHaveLength(2);
     expect(screen.getByRole('button')).toBeInTheDocument();
     expect(screen.getByLabelText('Password')).toBeInTheDocument();
     expect(screen.getByLabelText('Email address')).toBeInTheDocument();
     expect(screen.getByPlaceholderText('Password')).toBeInTheDocument();
     expect(screen.getByPlaceholderText('Email address')).toBeInTheDocument();
+    const textBox = screen.getByRole('textbox', {name: /Email address/i});
+    userEvent.type(textBox, 'Voronin');
+    expect(screen.getByDisplayValue(/voronin/i)).toBeVisible();
   });
 });
