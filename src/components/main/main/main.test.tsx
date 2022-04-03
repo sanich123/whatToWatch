533e4313b75edd1f1db04465f6a26d2f8e6e5454
sanich123 <aovoronin.piano@gmail.com>
2022-04-03 11:40:57 +0300

    Wrote two tests on handling error response

--- src/components/main/main/main.test.tsx
+++ src/components/main/main/main.test.tsx
@@ -1,6 +1,9 @@
 import { renderWithProviders } from '../../../test/test-utils';
 import Main from './main';
 import { screen } from '@testing-library/react';
+import { server } from '../../../test/server/index';
+import { rest } from 'msw';
+import { rootUrl, serverPath } from '../../../utils/const';
 
 describe('Main component', () => {
   it('Correctly handle with server data', async () => {
@@ -9,5 +12,13 @@ describe('Main component', () => {
     expect(await screen.findByText(/catalog/i)).toBeInTheDocument();
     expect(await screen.findByRole('button', {name: /show more/i})).toBeInTheDocument();
     expect(await screen.findAllByRole('link')).toHaveLength(20);
+  });
+
+  it('Correctly handle error response', async () => {
+    server.use(rest.get(`${rootUrl}${serverPath.films}`, (req, res, ctx) => res(ctx.status(404))));
+    renderWithProviders(<Main />);
+    expect(screen.getByText(/loading../i)).toBeInTheDocument();
+    expect(
+      await screen.findByText(/не удалось получить список фильмов с сервера/i)).toBeInTheDocument();
   });
 });
--- src/components/my-list/my-list.test.tsx
+++ src/components/my-list/my-list.test.tsx
@@ -1,6 +1,9 @@
 import { screen } from '@testing-library/react';
 import Favorites from './my-list';
 import { renderWithProviders} from '../../test/test-utils';
+import { rest } from 'msw';
+import { server } from '../../test/server';
+import { rootUrl, serverPath } from '../../utils/const';
 
 describe('MyList component', () => {
   it('correctly renders with data from server', async () => {
@@ -10,5 +13,14 @@ describe('MyList component', () => {
     expect(await screen.findByText(/catalog/i)).toBeInTheDocument();
     expect(await screen.findByText(/gangs of new york/i)).toBeInTheDocument();
     expect(await screen.findByRole('img', {name: /gangs of new york/i})).toBeInTheDocument();
+  });
+
+  it('Correctly handle error response', async () => {
+    server.use(rest.get(`${rootUrl}${serverPath.favorite}`, (req, res, ctx) => res(ctx.status(401))));
+    renderWithProviders(<Favorites />);
+    expect(screen.getByText(/loading../i)).toBeInTheDocument();
+    expect(
+      await screen.findByText(/sign in/i),
+    ).toBeVisible();
   });
 });
